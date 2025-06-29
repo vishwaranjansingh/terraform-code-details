56fc1f445a8f6b45d4489fa83e8772c10ba354e6
Vaidehi-AWS <viswaranjan55@gmail.com>
2025-06-29 17:12:02 +0530

    test and stroy added to the resource

--- child/Resource_group/main.tf
+++ child/Resource_group/main.tf
@@ -1,3 +1,21 @@
+# This Terraform configuration defines an Azure Resource Group resource.
+# It uses the "for_each" meta-argument to create multiple resource groups
+# based on the "var.rg" variable, which should be a map or object containing
+# the name and location for each resource group.
+# Each resource group will be created with the specified name and location.
+
+# Example usage:
+# variable "rg" {
+#   type = map(object({
+#     name     = string
+#     location = string
+#   }))
+# }
+
+# The resource block below will iterate over each entry in var.rg and
+# create a resource group accordingly.
+
+
 resource "azurerm_resource_group" "rgs" {
     for_each = var.rg
     name = each.value.name
