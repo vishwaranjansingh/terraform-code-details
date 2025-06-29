cc9dd5a50ea19ddb5fbc253a57661e9fdc947d3d
Vaidehi-AWS <viswaranjan55@gmail.com>
2025-06-29 16:56:45 +0530

    code added in child resource group

--- child/Resource_group/main.tf
+++ child/Resource_group/main.tf
@@ -4,3 +4,10 @@ resource "azurerm_resource_group" "rgs" {
     location = each.value.location
   
 }
+
+resource "azurerm_resource_group" "rgs" {
+    for_each = var.rg
+    name = each.value.name
+    location = each.value.location
+  
+}
